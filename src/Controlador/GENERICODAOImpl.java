0530db1bfd25e1d606483eae3ebef3421dad4041
jaumeferDAM <jaumefernandezacanmas@gmail.com>
2017-04-25 20:35:50 +0200

    ik

--- src/Controlador/GENERICODAOImpl.java
+++ src/Controlador/GENERICODAOImpl.java
@@ -7,41 +7,66 @@ package Controlador;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.hibernate.SessionFactory;
 
 public class GENERICODAOImpl<T, ID extends Serializable> implements GENERICODAO<T, ID> {
-    
-    SessionFactory sessionFactory;
-  
-    private final static Logger LOGGER = Logger.getLogger(GENERICODAOImpl.class.getName());
 
+    SessionFactory sessionFactory;
+
+    private final static Logger LOGGER = Logger.getLogger(GENERICODAOImpl.class.getName());
+    EntityManagerFactory emf;
+    EntityManager em;
     @Override
     public T crear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         emf = Persistence.createEntityManagerFactory("INTERNA");
+         em = emf.createEntityManager();
+        return (T) em;
     }
 
     @Override
     public void insertar(T entity) {
+        // Recupera el entity manager
+        // El persistim a la base de dades
+//        em.getTransaction().begin();
+        EntityTransaction et = em.getTransaction();
+
+        System.out.println("begin");
+        et.begin();
+
+        System.out.println("persist");
+        em.persist(entity);
+
+        System.out.println("commit");
+        //em.getTransaction().commit();
+        et.commit();
+
+        System.out.println("close");
+        em.close();
+    }
+}
+
+@Override
+        public void actualizar(T entity) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void actualizar(T entity) {
+        public T get(ID id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public T get(ID id) {
+        public void eliminar(ID id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void eliminar(ID id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public ArrayList<T> listarTodos() {
+        public ArrayList<T> listarTodos() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
